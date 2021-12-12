572c58d1fde82650fef5ac863fd8a029ff173406
Thomas McNish <mcnishthomas@gmail.com>
2021-12-12 09:22:07 -0500

    initial commit. Created multidimensional array variable

--- 3 - Multidimensional Arrays/3 - Build a Quiz Challenge/start/js/quiz.js
+++ 3 - Multidimensional Arrays/3 - Build a Quiz Challenge/start/js/quiz.js
@@ -1,10 +1,15 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
+const quiz = [
+  ['What kind of dog is Chewy', 'Cavapoo'],
+  ['How old is Chewy', '3'],
+  ['Will the Lions win this week?', 'No']]
 
+]
 
 // 2. Store the number of questions answered correctly
 
 
-/* 
+/*
   3. Use a loop to cycle through each question
       - Present each question to the user
       - Compare the user's response to answer in the array
